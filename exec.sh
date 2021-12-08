52f06c1c1d8e4688c4299f1d4658f85bfd2ae4eb
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2021-12-08 22:52:14 +0100

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,5 +1,7 @@
 rm -rf /opt/webcordautoupdater
 touch ~/opt/webcord/webcordautoupdater.txt
+wget -qO - https://api.github.com/repos/SpacingBat3/WebCord/releases/latest \
+| ver= grep tag_name \
 echo $ver >> /opt/webcord/webcordautoupdater.txt
 mkdir /opt/webcord
 wget https://raw.githubusercontent.com/MiMillieuh/Webcord-Wayland/main/webcord.sh -O /opt/webcord/updater.sh
@@ -9,6 +11,6 @@ mkdir /tmp/webcord
 | grep x86_64.rpm\
 | cut -d  '"' -f 4  \
 | wget -cqi - -O /tmp/webcord/webcord.rpm
-sudo rpm -ivh /tmp/webcord/webcord.rpm
+sudo rpm -ivhU /tmp/webcord/webcord.rpm
 rm -rf /tmp/webcord
 wget https://raw.githubusercontent.com/MiMillieuh/Webcord-Wayland/main/webcord.desktop -O /usr/share/applications/webcord.desktop
